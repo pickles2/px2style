0e9932a0ad967cc9b75a231b27e52b49598e9ecb
Tomoya Koyanagi <tomk79@gmail.com>
2020-06-13 12:58:11 +0900

    JSのリンクに関する不整合を修正

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /**
  * px2style/index.js
  */
-module.exports = function(Px2style){
-    var Px2style = function(){};
-    var modal = require('./src/modal/_modal.js')(Px2style);
-    var notice = require('./src/notice/_notice.js')(Px2style);
-    var loading = require('./src/styles/_loading.js')(Px2style);
-    var header = require('./src/header/_header.js')(Px2style);
-    return Px2style;
+module.exports = function(){
 }
+
+require('./src/modal/_modal.js')(module.exports);
+require('./src/notice/_notice.js')(module.exports);
+require('./src/styles/_loading.js')(module.exports);
+require('./src/header/_header.js')(module.exports);
