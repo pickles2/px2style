d8268614ffe244b70dacc15586b947c5d71209e4
Tomoya Koyanagi <tomk79@gmail.com>
2023-04-29 11:28:59 +0900

    インタラクティブモジュール について、テーマとベースの分離点の調整をした。

--- broccoli_modules/basics/interactives/accordion/module.js
+++ broccoli_modules/basics/interactives/accordion/module.js
@@ -6,17 +6,16 @@
 	window.px2style.registerInitFunction('px2-accordion', function(){
 		var accordionModules = document.querySelectorAll('.px2-accordion');
 		for(var idx = 0; idx < accordionModules.length; idx ++){
-			// console.log(idx);
+
 			var units = accordionModules[idx].querySelectorAll(':scope > .px2-accordion__unit');
-			for(var idx2 = 0; idx2 < units.length; idx2 ++){
-				units[idx2].classList.remove('px2-accordion--active');
-			}
+			// for(var idx2 = 0; idx2 < units.length; idx2 ++){
+			// 	units[idx2].classList.remove('px2-accordion--active');
+			// }
 
 			for(var idxUnit = 0; idxUnit < units.length; idxUnit ++){
 				var a = units[idxUnit].querySelector(':scope > .px2-accordion__label > a');
 				var content = units[idxUnit].querySelector(':scope > .px2-accordion__content');
 				(function(idxUnit, unit, units, a, content){
-					// console.log(idxUnit);
 					window.px2style.$(a)
 						.off('click.px2-accordion')
 						.on('click.px2-accordion', function(e){
@@ -28,7 +27,6 @@
 						});
 				})(idxUnit, units[idxUnit], units, a, content);
 			}
-			// console.log(units);
 		}
 	});
 })();
--- styleguide/common/scripts/contents.js
+++ styleguide/common/scripts/contents.js
@@ -59,17 +59,16 @@ try{
 	window.px2style.registerInitFunction('px2-accordion', function(){
 		var accordionModules = document.querySelectorAll('.px2-accordion');
 		for(var idx = 0; idx < accordionModules.length; idx ++){
-			// console.log(idx);
+
 			var units = accordionModules[idx].querySelectorAll(':scope > .px2-accordion__unit');
-			for(var idx2 = 0; idx2 < units.length; idx2 ++){
-				units[idx2].classList.remove('px2-accordion--active');
-			}
+			// for(var idx2 = 0; idx2 < units.length; idx2 ++){
+			// 	units[idx2].classList.remove('px2-accordion--active');
+			// }
 
 			for(var idxUnit = 0; idxUnit < units.length; idxUnit ++){
 				var a = units[idxUnit].querySelector(':scope > .px2-accordion__label > a');
 				var content = units[idxUnit].querySelector(':scope > .px2-accordion__content');
 				(function(idxUnit, unit, units, a, content){
-					// console.log(idxUnit);
 					window.px2style.$(a)
 						.off('click.px2-accordion')
 						.on('click.px2-accordion', function(e){
@@ -81,7 +80,6 @@ try{
 						});
 				})(idxUnit, units[idxUnit], units, a, content);
 			}
-			// console.log(units);
 		}
 	});
 })();
