db43a09a0b058e02d2b0bd9485302d486b6f66bd
Tomoya Koyanagi <tomk79@gmail.com>
2024-08-07 19:46:38 +0900

    image slider module: スライド番号を表示するオプションを追加

--- src_broccoli/modules/basics/interactives/image-slider/module.js
+++ src_broccoli/modules/basics/interactives/image-slider/module.js
@@ -10,6 +10,11 @@
         const $imageSlider = $('.px2-image-slider');
         $imageSlider.each((index, item)=>{
             const $module = $(item);
+            if( $module.data('slider') ){
+                // 2重に初期化させない。
+                return;
+            }
+            const $slideNumber = $module.find('.px2-image-slider__slide-number');
             const $container = $module.find('.px2-image-slider__container');
             $container.find('>li').addClass('px2-image-slider__slide');
 
@@ -25,19 +30,27 @@
                         perView: "auto",
                         spacing: 10,
                     },
+                    slideChanged: (slide) => {
+                        updateSlideInfo(slide.track.details.abs, slide.slides.length);
+                    },
                     created: () => {
                     },
-                },
-                [
-                    // add plugins here
-                ]
+                }
             );
+            $module.data('slider', slider);
             $module.find('.px2-image-slider__btn-prev').on('click', function(event){
                 slider.prev();
             });
             $module.find('.px2-image-slider__btn-next').on('click', function(event){
                 slider.next();
             });
+
+            function updateSlideInfo(currentSlide, totalSlides) {
+                const content = `${currentSlide%totalSlides + 1} / ${totalSlides}`;
+                $slideNumber.text(content);
+            }
+
+            slider.emit('slideChanged');
         });
 	});
 
