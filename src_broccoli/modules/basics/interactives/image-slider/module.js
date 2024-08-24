60f3a175d27d4a1e745e45dede5c39b865f065e7
Tomoya Koyanagi <tomk79@gmail.com>
2024-08-25 00:34:16 +0900

    image slider module: 内部コード修正

--- src_broccoli/modules/basics/interactives/image-slider/module.js
+++ src_broccoli/modules/basics/interactives/image-slider/module.js
@@ -5,9 +5,11 @@
 
 	const $ = require('jquery');
 	const KeenSlider = require('keen-slider').default;
+	const prefix = 'px2-image-slider';
+	const dataPrefix = 'data-px2-slider';
 
-	window.px2style.registerInitFunction('px2-image-slider', function(){
-		const $imageSlider = $('.px2-image-slider');
+	window.px2style.registerInitFunction(`${prefix}`, function(){
+		const $imageSlider = $(`.${prefix}`);
 		$imageSlider.each((index, module)=>{
 			initSlider($(module));
 		});
@@ -23,17 +25,17 @@
 			// 2重に初期化させない。
 			return;
 		}
-		const $slideNumber = $module.find('.px2-image-slider__slide-number');
-		const $sliderContainer = $module.find('.px2-image-slider__container');
+		const $slideNumber = $module.find(`.${prefix}__slide-number`);
+		const $sliderContainer = $module.find(`.${prefix}__container`);
 
-		const isLoop = !!$module.hasClass('px2-image-slider--is-loop');
+		const isLoop = !!$module.hasClass(`${prefix}--is-loop`);
 
-		const $slides = $sliderContainer.find('>li');
+		const $slides = $sliderContainer.find(`>li`);
 		if( !$slides.length ){
-			$sliderContainer.append('<li><div class="px2-image-slider__no-image">NO IMAGE</div></li>');
+			$sliderContainer.append(`<li><div class="${prefix}__no-image">NO IMAGE</div></li>`);
 		}
 
-		$slides.addClass('px2-image-slider__slide');
+		$slides.addClass(`${prefix}__slide`);
 
 		const slider = new KeenSlider(
 			$sliderContainer.get(0),
@@ -53,19 +55,19 @@
 			}
 		);
 		$module.data('slider', slider);
-		$module.find('.px2-image-slider__btn-prev').on('click', function(event){
+		$module.find(`.${prefix}__btn-prev`).on('click', function(event){
 			slider.prev();
 		});
-		$module.find('.px2-image-slider__btn-next').on('click', function(event){
+		$module.find(`.${prefix}__btn-next`).on('click', function(event){
 			slider.next();
 		});
 
-		$module.find('[data-px2-slider-rel="caption-link"] a').closest('.px2-image-slider__slide')
-			.addClass('px2-image-slider__slide--clickable')
+		$module.find(`[${dataPrefix}-rel="caption-link"] a`).closest(`.${prefix}__slide`)
+			.addClass(`${prefix}__slide--clickable`)
 			.on('click', function(event){
 				event.stopPropagation();
 				event.preventDefault();
-				const $link = $(this).find('[data-px2-slider-rel="caption-link"] a');
+				const $link = $(this).find(`[${dataPrefix}-rel="caption-link"] a`);
 				window.open($link.prop('href'), $link.prop('target') || '_self');
 			});
 
@@ -76,8 +78,8 @@
 			$slideNumber.text(content);
 
 			if( !isLoop ){
-				const $btnPrev = $module.find('.px2-image-slider__btn-prev');
-				const $btnNext = $module.find('.px2-image-slider__btn-next');
+				const $btnPrev = $module.find(`.${prefix}__btn-prev`);
+				const $btnNext = $module.find(`.${prefix}__btn-next`);
 				$btnPrev.prop('disabled', true);
 				$btnNext.prop('disabled', true);
 				if( currentSlide > 0 ){
@@ -90,9 +92,9 @@
 
 			slider.slides.forEach((slide, idx) => {
 				if (idx === slider.track.details.rel) {
-					slide.classList.remove("px2-image-slider__slide--inactive");
+					slide.classList.remove(`${prefix}__slide--inactive`);
 				} else {
-					slide.classList.add("px2-image-slider__slide--inactive");
+					slide.classList.add(`${prefix}__slide--inactive`);
 				}
 			});
 		}
